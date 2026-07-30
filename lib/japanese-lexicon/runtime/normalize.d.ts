3f729f638c6c8544e7c4a64a2e34e9baf29bf5b9
Logesh <loganlogesh17@gmail.com>
2026-07-30 15:58:08 +0900

    Type the vendored normalization runtime

---
+++ lib/japanese-lexicon/runtime/normalize.d.ts
@@ -0,0 +1,4 @@
+export function normalizeJapanese(value: unknown): string;
+export function canonicalSurface(kanji: unknown, kana: unknown): string;
+export function containsKanji(value: unknown): boolean;
+export function containsPunctuationOrSpace(value: unknown): boolean;
